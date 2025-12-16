a1de26dbcc56dafcadf36b5a0f11d8108b3e7e83
stefanolonghi <ste.longhii@gmail.com>
2025-12-16 18:48:12 +0100

    fixed file path to work also on deploy

--- src/loader.py
+++ src/loader.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import ast
 import streamlit as st
+from pathlib import Path
 
 @st.cache_data
 def load_data():
-    df_info = pd.read_csv("data/id_information_mmsr.tsv", sep="\t")
-    df_urls = pd.read_csv("data/id_url_mmsr.tsv", sep="\t")
-    df_genres = pd.read_csv("data/id_genres_mmsr.tsv", sep="\t")
+    HERE = Path(__file__).parent       # cartella dove si trova questo script
+    DATA = HERE / "data"               
+    df_info = pd.read_csv(DATA / "id_information_mmsr.tsv", sep="\t")
+    df_urls = pd.read_csv(DATA / "id_url_mmsr.tsv", sep="\t")
+    df_genres = pd.read_csv(DATA / "id_genres_mmsr.tsv", sep="\t")
     df_merged = df_info.merge(df_urls, on="id", how="left").merge(df_genres, on="id", how="left")
     return df_merged
 
